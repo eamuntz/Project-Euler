88893fae2f28a9c29a1f26ea2fc0054808126244
Eric Mundt <dirkgentlycommits@gmail.com>
2014-02-17 23:09:47 -0700

    Removed miller-rabin designed for py33

--- prime.py
+++ prime.py
@@ -17,32 +17,7 @@ if n < 2,152,302,898,747, it is enough to test a = 2, 3, 5, 7, and 11;
 if n < 3,474,749,660,383, it is enough to test a = 2, 3, 5, 7, 11, and 13;
 if n < 341,550,071,728,321, it is enough to test a = 2, 3, 5, 7, 11, 13, and 17.
 '''
-'''
-def millerRabin(n, k = 7):
-   """use Rabin-Miller algorithm to return True (n is probably prime)
-      or False (n is definitely composite)"""
-   if n < 6:  # assuming n >= 0 in all cases... shortcut small cases here
-      return [False, False, True, True, False, True][n]
-   elif n & 1 == 0:  # should be faster than n % 2
-      return False
-   else:
-      s, d = 0, n - 1
-      while d & 1 == 0:
-         s, d = s + 1, d >> 1
-      for a in random.sample(xrange(2, min(n - 2, sys.maxint)), min(n - 4, k)):
-         x = pow(a, d, n)
-         if x != 1 and x + 1 != n:
-            for r in xrange(1, s):
-               x = pow(x, 2, n)
-               if x == 1:
-                  return False  # composite for sure
-               elif x == n - 1:
-                  a = 0  # so we know loop didn't continue to end
-                  break  # could be strong liar, try another a
-            if a:
-               return False  # composite if we reached end of this loop
-      return True  # probably prime if reached end of outer loop
-'''
+
 
 def miller_rabin(m, k = 3):
     s=1
